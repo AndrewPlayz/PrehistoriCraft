6f4ad9d38758c43a54d18aed5dd6c9e4de491ec2
Andrew <AndrewPlayzVideos@gmail.com>
2016-04-03 00:41:03 +1000

    World Gen Fixed.

--- src/main/java/net/andrewplayz/prehistoricraft/server/world/PrehistoriCraftWorldGen.java
+++ src/main/java/net/andrewplayz/prehistoricraft/server/world/PrehistoriCraftWorldGen.java
@@ -4,27 +4,13 @@ import net.andrewplayz.prehistoricraft.server.block.PhCBlockRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkGenerator;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraftforge.fml.common.IWorldGenerator;
 import java.util.Random;
 
 public class PrehistoriCraftWorldGen implements IWorldGenerator {
-
-    @Override
-    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
-        switch (world.provider.getDimension()) {
-            case 0:
-                //Generate the Over World
-                generateSurface(world, random, chunkX * 16, chunkZ * 16);
-            case -1:
-                //Generate the Nether
-                generateNether(world, random, chunkX * 16, chunkZ * 16);
-            case 1:
-                //Generate the End
-                generateEnd(world, random, chunkX * 16, chunkZ * 16);
-        }
-    }
 
     private void generateSurface(World world, Random random, int x, int z) {
         //this.addOreSpawn(PrehistoriCraft.Ore/Block, world, random, blockXPos, blockZPos, maxX, maxZ, maxVeinSize, ChanceToSpawn, minY, MaxY);
@@ -47,6 +33,21 @@ public class PrehistoriCraftWorldGen implements IWorldGenerator {
             int posY = minY + random.nextInt(MaxY - minY);
             int posZ = blockZPos + random.nextInt(maxZ);
             (new WorldGenMinable(block.getDefaultState(), maxVeinSize)).generate(world, random, new BlockPos(posX, posY, posZ));
+        }
+    }
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
+        switch (world.provider.getDimension()) {
+            case 0:
+                //Generate the Over World
+                generateSurface(world, random, chunkX * 16, chunkZ * 16);
+            case -1:
+                //Generate the Nether
+                generateNether(world, random, chunkX * 16, chunkZ * 16);
+            case 1:
+                //Generate the End
+                generateEnd(world, random, chunkX * 16, chunkZ * 16);
         }
     }
 }
