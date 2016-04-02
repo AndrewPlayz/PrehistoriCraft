cf0763e20c6f2975b120cc483b22f68eb28a3917
Andrew <AndrewPlayzVideos@gmail.com>
2016-04-02 21:54:47 +1000

    Small fix...

--- src/main/java/net/andrewplayz/prehistoricraft/server/entity/EntityGender.java
+++ src/main/java/net/andrewplayz/prehistoricraft/server/entity/EntityGender.java
@@ -3,19 +3,19 @@ package net.andrewplayz.prehistoricraft.server.entity;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
 
-public enum EntityGender {
+public enum EntityGender{
     MALE,
     FEMALE;
 
     public void writeToNBT(Entity entity, NBTTagCompound compound) {
-        compound.setInteger("Gender", entity.EntityDataManager().getWatchableObjectInt(23));
+        compound.setInteger("Gender", entity.getDataWatcher().getWatchableObjectInt(23));
     }
 
     public static void readFromNBT(Entity entity, NBTTagCompound compound) {
-        entity.EntityDataManager().updateObject(23, compound.getInteger("Gender"));
+        entity.getDataWatcher().updateObject(23, compound.getInteger("Gender"));
     }
 
     public static EntityGender getGender(Entity entity) {
-        return entity.EntityDataManager().getWatchableObjectInt(23) == 0 ? MALE : FEMALE;
+       return entity.getDataWatcher().getWatchableObjectInt(23) == 0 ? MALE : FEMALE;
     }
 }
--- src/main/java/net/andrewplayz/prehistoricraft/server/entity/EntityPrehistoric.java
+++ src/main/java/net/andrewplayz/prehistoricraft/server/entity/EntityPrehistoric.java
@@ -2,21 +2,25 @@ package net.andrewplayz.prehistoricraft.server.entity;
 
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.world.World;
-
 import java.util.Random;
 
 public class EntityPrehistoric extends EntityCreature {
-    public float randomMobScale = new Random().nextFloat() + 1;
+    public float randomMobScale = new Random().nextFloat() + 0.3F;
 
     public EntityPrehistoric(World world) {
         super(world);
     }
 
+    private static final DataParameter<Boolean> x = EntityDataManager.createKey(EntityPrehistoric.class, DataSerializers.BOOLEAN);
+
+    @Override
     protected void entityInit() {
         super.entityInit();
-        getDataManager().addObject(23, rand.nextInt(2));
+        dataWatcher.register(x, false);
     }
 
     public void writeEntityToNBT(NBTTagCompound compound) {
@@ -28,8 +32,9 @@ public class EntityPrehistoric extends EntityCreature {
         super.readEntityFromNBT(compound);
         EntityGender.readFromNBT(this, compound);
     }
-    
+
     public EntityGender getGender() {
         return EntityGender.getGender(this);
     }
 }
+
