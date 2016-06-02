4621c71c3e88643b3e29eaf36c42e4db0f9a4420
iLexiconn <me@ilexiconn.net>
2016-06-02 11:47:36 +0200

    Revert "Fixed block rotation"

--- src/main/java/net/andrewplayz/prehistoricraft/client/renderer/RenderLaptop.java
+++ src/main/java/net/andrewplayz/prehistoricraft/client/renderer/RenderLaptop.java
@@ -15,14 +15,8 @@ public class RenderLaptop extends TileEntitySpecialRenderer<TileEntityLaptopBloc
         this.model = new ModelLaptop();
     }
 
-	private float getRotation(TileEntityLaptopBlock tile) {
-		switch(tile.getBlockMetadata()){
-		default: return 0;
-		case 1: return 90;
-		case 2: return 180;
-		case 3: return -90;
-		}
-	}
+    public static void render(){
+    }
 
     @Override
     public void renderTileEntityAt(TileEntityLaptopBlock tileentity, double x, double y, double z, float u, int v) {
@@ -30,7 +24,6 @@ public class RenderLaptop extends TileEntitySpecialRenderer<TileEntityLaptopBloc
         GL11.glTranslatef(0f, 0f,0f);
         GL11.glTranslated((float)x + 0.5F, (float)y + 1.5F, (float)z + 0.5F);
         GL11.glRotatef(180, 0F, 0F, 1F);
-        GL11.glRotatef(this.getRotation(tile), 0.0F, 1.0F, 0.0F);
         GL11.glPushMatrix();
         this.bindTexture(texture);
         this.model.render(null, 0, 0, 0, 0, 0, 0.0625F);
